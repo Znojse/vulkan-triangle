4df7eb5e04a37248b081c677f5f4d138ccd4da8c
Alexander Olsson <alexander.olsson.ext@svpworldwide.com>
2025-08-21 15:35:11 +0200

    Project now builds with clang and ninja, fixed a profile for windows.

--- README.md
+++ README.md
@@ -2,9 +2,27 @@
 Game Engine that will be developed with C++, Vulkan and structured by Conan and CMake
 
 # Compilation
+## Windows
+### conan build Release
+`conan build . --profile:a ./conan-profiles/win64-clang`
+
+### bash
 ```bash
-rm -rf build &&
-conan install --output-folder=build --profile:a default --build=missing -s build_type=Debug . &&
-conan install --output-folder=build --profile:a default --build=missing -s build_type=Release . &&
+clear &&
+rm -rf build/* &&
+conan install --profile:a ./conan-profiles/win64-clang --build=missing -s build_type=Debug . &&
+conan install --profile:a ./conan-profiles/win64-clang --build=missing -s build_type=Release . &&
 cmake --workflow --preset=vtAll
+```
+### Powershell
+```powershell
+clear ; if (Test-Path -LiteralPath build/) { Remove-Item build/* -Recurse -Force } ; conan install --profile:a ./conan-profiles/win64-clang --build=missing -s build_type=Debug . ; conan install --profile:a ./conan-profiles/win64-clang --build=missing -s build_type=Release . ; cmake --workflow --preset=vtAll
+```
+
+# Run
+## Windows
+```bash
+./build/vtDefault/Release/vulkan-triangle_C.exe
+# or
+./build/vtDefault/Release/vulkan-triangle.exe
 ```
--- conanfile.py
+++ conanfile.py
@@ -1,58 +1,37 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
 
 class VulkanTriangleRecipe(ConanFile):
+    name = "Vulkan Triangle"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps"
-    # generators = "CMakeToolchain", "CMakeDeps"
 
     def requirements(self):
-        self.requires("catch2/3.9.1")
+        pass
 
     def build_requirements(self):
-        pass
+        self.requires("ninja/1.13.1")
+        self.requires("mingw-builds/15.1.0")
+        self.requires("catch2/3.9.1")
+        self.requires("cmake/[>=3.29]")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        # cmake.test()
+        # self.run("mybuildsystem . --configure")
+        # self.run("mybuildsystem . --build")
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = 'ConanPresets.json'
+        tc.generate()
 
     def layout(self):
         cmake_layout(self)
 
-# import os
-
-# from conan import ConanFile
-# from conan.tools.cmake import cmake_layout
-# from conan.errors import ConanInvalidConfiguration
-# # from conan.tools.files import copy
-# from conan.tools.cmake import CMake
-
-# class CompressorRecipe(ConanFile):
-#     settings = "os", "compiler", "build_type", "arch"
-#     generators = "CMakeToolchain", "CMakeDeps"
-
-#     def requirements(self):
-#         self.requires("zlib/1.3.1")
-
-#         # Add base64 dependency for Windows
-#         if self.settings.os == "Windows":
-#             self.requires("base64/0.4.0")
-
-#     def build_requirements(self):
-#         # Use the system's CMake for Windows
-#         if self.settings.os != "Windows":
-#             self.tool_requires("cmake/3.27.9")
-
-#     def layout(self):
-#         cmake_layout(self)
-
-#     def validate(self):
-#         if self.settings.os == "Macos" and self.settings.arch == "armv8":
-#             raise ConanInvalidConfiguration("ARM V8 not supported in MacOS")
-
-#     # def generate(self):
-#     #     # Copy all resources from the dependency's resource directory
-#     #     # to the "assets" folder in the source directory of your project
-#     #     dep = self.dependencies["dep_name"]
-#     #     copy(self, "*", dep.cpp_info.resdirs[0], os.path.join(self.source_folder, "assets"))
-
-#     def build(self):
-#         cmake = CMake(self)
-#         cmake.configure()
-#         cmake.build()
+# Example functions that can be added:
+#   - validate, generate ...
